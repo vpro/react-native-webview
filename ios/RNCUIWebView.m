089c9dc6ddc68cfdeb7dd1add33ef5761990456f
frenkie <frank@frankbosma.nl>
2018-12-24 13:50:14 +0100

    BC-152

--- ios/RNCUIWebView.m
+++ ios/RNCUIWebView.m
@@ -32,7 +32,6 @@ static NSString *const kPostMessageHost = @"postMessage";
 - (void)dealloc
 {
   _webView.delegate = nil;
-  _webView.uiDelegate = nil;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame
@@ -43,7 +42,6 @@ static NSString *const kPostMessageHost = @"postMessage";
     _contentInset = UIEdgeInsetsZero;
     _webView = [[UIWebView alloc] initWithFrame:self.bounds];
     _webView.delegate = self;
-    _webView.uiDelegate = self;
 #if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 110000 /* __IPHONE_11_0 */
     if ([_webView.scrollView respondsToSelector:@selector(setContentInsetAdjustmentBehavior:)]) {
       _webView.scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
